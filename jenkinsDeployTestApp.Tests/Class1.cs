fc3891e57880abd39295eb1405b3929fce7e13d3
wparrish <wparrish@myvrx.com>
2017-07-12 09:54:24 -0600

    initial commit

---
+++ jenkinsDeployTestApp.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace jenkinsDeployTestApp.Tests
+{
+    public class TestClass
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
